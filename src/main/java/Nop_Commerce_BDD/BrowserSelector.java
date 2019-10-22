391d3325693dd3c0805ea9d85e6435e8b1de8c18
ajitsariakarishma <karishmaajitsaria@gmail.com>
2019-10-22 19:38:41 +0100

    commit

--- src/main/java/Nop_Commerce_BDD/BrowserSelector.java
+++ src/main/java/Nop_Commerce_BDD/BrowserSelector.java
@@ -3,13 +3,17 @@ package Nop_Commerce_BDD;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class BrowserSelector extends Utils {
     LoadProperties props = new LoadProperties();
 
     public void setUpBrowser() {
         //Storing the value of browser from config file in a String Variable
-        String browser = props.getProperty("browser");
+        String browser = System.getProperty("browser");
         if (browser.equalsIgnoreCase("chrome")) {
 
             //setting system properties to launch chrome browser
@@ -28,16 +32,29 @@ public class BrowserSelector extends Utils {
 
 
         } else if (browser.equalsIgnoreCase("firefox")) {
-            System.setProperty("webdriver.gecko.driver", "src\\test\\java\\Resources\\WebBrowsers\\geckodriver.exe");
-            driver = new FirefoxDriver();
+            //DesiredCapabilities ff = DesiredCapabilities.firefox();
+           // ff.acceptInsecureCerts();
+            //ff.setAcceptInsecureCerts(true);
+           // FirefoxOptions options = new FirefoxOptions();
+           // options.merge(ff);
+           System.setProperty("webdriver.gecko.driver", "src\\test\\java\\Resources\\WebBrowsers\\geckodriver.exe");
+            driver = new FirefoxDriver();//tried to merge ff here but didn't work
+        }
+            else if(browser.equalsIgnoreCase("IE")){
+                System.setProperty("webdriver.ie.driver","src\\test\\java\\Resources\\WebBrowsers\\IEDriverServer.exe");
+                InternetExplorerOptions internetExplorerOptions=new InternetExplorerOptions();
+                internetExplorerOptions.setCapability(InternetExplorerDriver.IE_SWITCHES,"-private");
+                internetExplorerOptions.setCapability(InternetExplorerDriver.IE_ENSURE_CLEAN_SESSION,true);
+                driver= new InternetExplorerDriver(internetExplorerOptions);}
 
-        } else {
 
-            System.out.println("Browser name is either empty or typed wrong :" + browser);
+             else {
+
+            System.out.println("Browser name is either empty or typed wrong :" + browser);}
 
         }
     }
-}
+
 
 
 

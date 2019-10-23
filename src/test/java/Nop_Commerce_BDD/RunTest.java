7f66392dc18471230b2f64ee72697876ae1f3684
ajitsariakarishma <karishmaajitsaria@gmail.com>
2019-10-23 19:04:15 +0100

    commit

--- src/test/java/Nop_Commerce_BDD/RunTest.java
+++ src/test/java/Nop_Commerce_BDD/RunTest.java
@@ -3,9 +3,14 @@ package Nop_Commerce_BDD;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
 import org.junit.runner.RunWith;
+import org.testng.annotations.Test;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(features=".", tags="@regression")
-public class RunTest {
+//@RunWith(Cucumber.class)
+@CucumberOptions(features=".", tags=("@register"),plugin={"pretty","html:target/Destinations"})
+@Test
+public class RunTest extends AbstractTestNGCucumberTests {
+
+
 }
